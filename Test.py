42a1683bf05b73f30b28dfedfe847512edad3017
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-17 15:38:45 +0530

    17/08/20

--- Test.py
+++ Test.py
@@ -1,7 +1,1 @@
-import heapq
-
-employee = [(7, 'bharath'), (8, 'pradeep'), (3, 'prabha'), (4, 'chitra'),
-            (9, 'akesh'), (6, 'raj'), (1, 'ram'), (2, 'kumar')]
-print(employee)
-heapq.heapify(employee)
-print(employee)
+print('hell')
